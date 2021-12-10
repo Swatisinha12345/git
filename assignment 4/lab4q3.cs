e6498bd4be222281ed33ed968f1b487534063ea7
Swatisinha12345 <94735768+Swatisinha12345@users.noreply.github.com>
2021-12-10 13:03:26 +0530

    assignment 4
    submitted assignment4

---
+++ assignment 4/lab4q3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4_q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 50)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
